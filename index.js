2052fd931a3f0e4bda9c7f0f0dfc6bb90fb45a51
Ripika Sharma <ripikasharma@gmail.com>
2023-09-11 16:09:09 -0400

    functioncont

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(activity ="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+
+}
+mondayWork();
+
+function wrapAdjective(highlight) {
+    return function(adjective) {
+        return `You are ${highlight}${adjective}${highlight}!`
+    };
+}
+
+const wrappedAdjective = wrapAdjective("*");
+const result = wrappedAdjective("a hard worker");
+console.log(result);
+
+const wrappedAdjective1 = wrapAdjective("||");
+const result1 = wrappedAdjective1("a dedicated programmer");
+console.log(result1);
